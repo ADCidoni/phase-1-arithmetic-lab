b32e126e232a14eaa45804cd4dc1b5cd222069e9
April <a.d.cidoni@gmail.com>
2023-11-19 15:16:42 -0500

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 2
+let num2 = 31
+let multiply  = (num1 * num2)
+let random = num2
+Number.isInteger(random)
+let num3 = 16
+let num4 = 4
+let mod = (num4 % num3)
+let max = Math.max(4, 8, 12, 16, 20)
